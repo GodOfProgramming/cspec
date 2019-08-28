7ab34aeac196d2a1539165daebbd9c1c7a099162
GodOfProgramming <jon.p.dickert@gmail.com>
2019-08-27 20:18:35 -0400

    auto update -> moved matchers into their own files

--- examples/second.spec.cpp
+++ examples/second.spec.cpp
@@ -4,6 +4,7 @@ BeginSpec(spec3);
   Describe("A second spec", [] {
     It("Fails so I can see the output", [] {
       Expect("foo").toEqual("bar");
+      Expect(1).toEqual(2);
     });
   });
 EndSpec();
