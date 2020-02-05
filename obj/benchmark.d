2d357924e6bbfa8b5e5d2aa775660b23a039c7de
Jonathan Dickert <jon.p.dickert@gmail.com>
2020-02-04 20:46:38 -0500

    deleted d files

--- obj/benchmark.d
+++
@@ -1,6 +0,0 @@
-obj/benchmark.o: src/benchmark.cpp include/benchmark.hpp \
- include/clock.hpp
-
-include/benchmark.hpp:
-
-include/clock.hpp:
--- obj/evaluation.d
+++
@@ -1,6 +0,0 @@
-obj/evaluation.o: src/evaluation.cpp include/evaluation.hpp \
- include/misc.hpp
-
-include/evaluation.hpp:
-
-include/misc.hpp:
